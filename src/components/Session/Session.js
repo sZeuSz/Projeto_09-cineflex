7701f6f10449abf68db86c145695552d21f66cff
zeus.k99@gmail.com <zeus.k99@gmail.com>
2021-09-05 11:21:24 -0400

    ajeitei layout do footer dos assentos

--- src/components/Session/Session.js
+++ src/components/Session/Session.js
@@ -7,10 +7,10 @@ import "./Session.css"
 export default function Session () {
 
     const [seats, setSeats] = useState(null);
-    const {idSessao} = useParams();
     const [nome, setNome] = useState("");
     const [cpf, setCpf] = useState("");
-    const [selected, setSelected] = useState("");
+    const [selectedSeats, setSelectedSeats] = useState([]);
+    const {idSessao} = useParams();
 
     useEffect ( () => {
 
@@ -26,12 +26,11 @@ export default function Session () {
         <main className="container ">
             <h2 className="centralize-items-h">Selecione o(s) assento(s)</h2>
             <section className="seats">
-                {seats === null ? "" : seats.seats.map((seat, index) => {return <Button key={index} seat={seat} />})}
-            
+                {seats === null ? "" : seats.seats.map((seat, index) => {return <Button key={index} seat={seat} setSelectedSeats={setSelectedSeats} selectedSeats={selectedSeats} />})}
             </section>
             <div className="states">
                 <div className="info-states">
-                    <button className="seat selecionado"></button>
+                    <button className="seat selected"></button>
                     <span>Selecionado</span>
                 </div>
                 <div className="info-states">
@@ -39,7 +38,7 @@ export default function Session () {
                     <span>Disponivel</span>
                 </div>
                 <div className="info-states">
-                    <button className="seat indisponivel" ></button>
+                    <button className="seat unavailable" ></button>
                     <span>Indisponível</span>
                 </div>
             </div>
@@ -48,33 +47,51 @@ export default function Session () {
                 <input placeholder="Digite seu nome..." onChange={e => setNome(e.target.value)}></input>
                 <h2 className="centralize-items-h">CPF do comprador:</h2>
                 <input placeholder="Digite seu CPF..." onChange={e => setCpf(e.target.value)}></input>
+
+                <button onClick={() => imprime(selectedSeats)}>clica ni mim</button>
             </div>
             {nome}  
             {cpf}
             <footer>
                 <figure className="image">
-                    <img src={seats === null ? "" : seats.movie.posterURL} alt="movie-schedules" />
-                    <figcaption>{seats=== null ? "" : seats.movie.title}</figcaption>
+                    <div className="background-img centralize-items-h">
+                        <img src={seats === null ? "" : seats.movie.posterURL} alt="movie-schedules" />
+                    </div>
+                    <div className="column-style">
+                        <figcaption>{seats=== null ? "" : seats.movie.title}</figcaption>
+                        <figcaption>{seats=== null ? "" : (`${seats.day.weekday} - ${seats.day.date}`)}</figcaption>
+                    </div>
+
                 </figure>
             </footer> 
         </main>
     )
 }
+function imprime (selectedSeats){
+    console.log(selectedSeats)
+}
 
-function Button ({seat}) {
+function Button ({seat, selectedSeats, setSelectedSeats}) {
 
-    const [selecionado, setSelecionado] = useState("");
+    const [selected, setSelected] = useState("");
 
-    function selecionar() {
-    
-        if (selecionado === "") {
-          setSelecionado("selecionado");
-        } else {
-          setSelecionado("");
+    function select(isAvailable, id) {
+        
+        if(!isAvailable){
+            alert("Esse assento não está disponível");
+        }
+        else if (selected === "") {
+          console.log("auqi")
+          setSelectedSeats([...selectedSeats, id]);
+          setSelected("selected");
+        }
+        else {
+          setSelected("");
+          setSelectedSeats(selectedSeats.filter((seatId) => seatId !== id))
         }
     }
 
     return (
-        <button className={`seat ${seat.isAvailable === false ? "indisponivel" : selecionado}`} onClick={() => selecionar(seat.isAvailable)}>{seat.name}</button>
+        <button className={`seat ${seat.isAvailable === false ? "unavailable" : selected}`} onClick={() => select(seat.isAvailable,seat.id)}>{seat.name}</button>
     )
 }
